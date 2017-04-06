cb23899f49bcc503cdbef844a858b21178ea86bf
laivietanh1986 <laivietanh1986@gmail.com>
2017-04-06 16:37:30 +0700

    experiment Rabbit Mq

---
+++ hellowork/Receive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Receive
+    {
+        public static void Main (string[] args)
+        {
+            var factory = new ConnectionFactory(){HostName="localhost"};
+            using(var connection = factory.CreateConnection())
+            {
+                using(var chanel = connection.CreateModel())
+                {
+                    chanel.QueueDeclare(queue:"Hello",
+                                        exclusive:false,
+                                        autoDelete:false,
+                                        durable:false,
+                                        arguments:null);
+                    var consumer = new EventingBasicConsumer(chanel);
+                    consumer.Received +=(model,ea)=>{
+                        var body = ea.Body;
+                        var message = UnicodeEncoding.UTF8.GetString(body);
+                        System.Console.WriteLine($"Receive {message}");
+
+                    };
+                    chanel.BasicConsume(queue:"Hello",
+                                        noAck:true,
+                                        consumer:consumer);
+                    System.Console.WriteLine("input any key to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ hellowork/Send/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName="localhost";
+            using(var connection = factory.CreateConnection())
+            {
+                using(var chanel = connection.CreateModel())
+                {
+                    chanel.QueueDeclare(queue:"Hello",
+                                        durable:false,
+                                        exclusive:false,
+                                        autoDelete:false,
+                                        arguments:null);
+                    string message = "Hello world";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    chanel.BasicPublish(exchange:"",
+                                        routingKey:"Hello",
+                                        basicProperties:null,
+                                        body:body);
+                    System.Console.WriteLine($"Send {message}");
+                }
+            }
+            System.Console.WriteLine("enter key to exit ..");
+            Console.ReadLine();
+        }
+    }
+}
